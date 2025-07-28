dc793dca636bd9b81f918d2043f338d9fd5e9db8
gahdfrey <89304019+gahdfrey@users.noreply.github.com>
2025-07-28 14:43:31 +0100

    style(products/edit): adjust padding

--- app/products/edit/[id]/page.tsx
+++ app/products/edit/[id]/page.tsx
@@ -7,7 +7,11 @@ import { notFound } from "next/navigation";
 import MetaTags from "../../../components/SEO/MetaTags";
 import { use } from "react";
 
-export default function EditProduct({ params }: { params: Promise<{ id: string }> }) {
+export default function EditProduct({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const resolvedParams = use(params);
   const { products } = useProductStore();
   const router = useRouter();
@@ -20,7 +24,7 @@ export default function EditProduct({ params }: { params: Promise<{ id: string }
   return (
     <>
       <MetaTags />
-      <div className="container mx-auto py-8">
+      <div className="container mx-auto p-8">
         <h1 className="text-3xl font-bold mb-4">Edit Product</h1>
         <ProductForm
           initialProduct={product}
